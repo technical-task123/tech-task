7e17c10226f3e5222605e81131ecdc4859a89578
Анатолий Данчук <anatoliy.danchuk.softimus@gmail.com>
2018-03-27 00:49:58 +0300

    worked and sow-sow code

--- src/Lib/RunAction.php
+++ src/Lib/RunAction.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 namespace DanchukAS\AmadeusTechTask123\Lib;
 
 
+use DanchukAS\AmadeusTechTask123\AAction;
+
 class RunAction extends \LibFunction
 {
 
-    public function run($action)
+    public function run(AAction $action)
     {
 
         if ($this->lib->isAvailableActionRun($action)) {
             $initializer = $this->lib->base->createFunctionalObject($action->initializer);
             $initializer->run($action);
         } else {
-            $message = \print_r($action, true) . ' is not allowed with ' . \print_r($action->runParam, true);
+            $message = 'Action "' . $action->runParam->actionName
+                . '" is not available for run with next param: '
+                . \print_r($action->runParam, true);
             \trigger_error($message, E_USER_NOTICE);
         }
     }
