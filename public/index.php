3f787716dc0fec9cbd7ccc74d409a9ab78a27ad7
Анатолий Данчук <anatoliy.danchuk.softimus@gmail.com>
2018-03-27 18:14:16 +0300

    worked and sow-sow code

--- public/index.php
+++ public/index.php
@@ -1,46 +1,27 @@
 <?php
 declare(strict_types=1);
 
-use DanchukAS\AmadeusTechTask123\ILib;
+namespace DanchukAS\AmadeusTechTask123;
 
+// Only for auto complete by IDE.
+
+// Include composer's autoload as the only autoload for this project.
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /** @var ILib $lib */
-$lib = new \DanchukAS\AmadeusTechTask123\Lib();
+$lib = new Lib();
 
+// Load animal collection from task.
 $animal_collection = $lib->initAmadeusZooAnimalCollection();
 $zoo = $lib->base->createObject(\Zoo::class);
 $lib->setAnimalCollection($zoo, $animal_collection);
 
-ob_start();
+// For not mix error and output. Part 1/2.
+\ob_start();
 
 $lib->live($zoo);
 
-// for not mix error and output. often help, but not always.
-usleep(1000000);
-ob_end_flush();
-
-// @todo composer auto update
-// @todo composer.json max
-// @todo composer  "provide": {"DanchukAS/Zoo": "1.0.*"}
-// @todo docker
-// @todo phpunit
-// @todo service coverage
-// @todo service php-codesnifer analise
-// @todo add badge to readme
-// @todo add comment to functions
-// @todo add my explains to readme
-// @todo push 2 variant
-// @todo pullRequest to Amadeus
-// @todo documentation for ILibGenerator.php, LibFunction
-// @todo say Olga by skype
-// @todo add Phing with phpdoc
-
-//class Animal
-//{
-//    public function byte($object)
-//    {
-//        echo $this->name . ' has bitten' . $object;
-//    }
-//}
-
+// For not mix error and output. Part 2/2.
+// @todo set_error_handler
+\usleep(1000000);
+\ob_end_flush();
--- src/Lib.php
+++ src/Lib.php
@@ -10,7 +10,13 @@ declare(strict_types=1);
 namespace DanchukAS\AmadeusTechTask123;
 
 /**
- * @see \DanchukAS\AmadeusTechTask123\ILib
+ * Main point calling functions.
+ *
+ * Present in functions as property $lib.
+ *
+ * @see \DanchukAS\AmadeusTechTask123\ILib Methods are auto generated from files "Lib" directory.
+ *
+ * @todo Auto generate this file instead ILib
  */
 class Lib extends \ALib
 {
