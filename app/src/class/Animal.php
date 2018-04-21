de5a8b24b23e5586248e124cd36d26d9a9f37024
Igor S <igor.shp@i.ua>
2018-04-21 14:11:57 +0300

    divide application and class

---
+++ app/src/class/Animal.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Class Animal
+ */
+class Animal
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function walk()
+    {
+        if($this->name == 'dog' || $this->name == 'cat' || $this->name == 'rat')
+            echo $this->name . ' walking';
+    }
+
+    public function meow()
+    {
+        echo $this->name . ' meow';
+    }
+
+    public function run()
+    {
+        echo $this->name . ' run';
+    }
+
+    public function wuf()
+    {
+        echo $this->name . ' wuf';
+    }
+
+    public function byte($object)
+    {
+        echo $this->name . ' has bitten' . $object;
+    }
+
+    public function fly()
+    {
+        echo $this->name . ' fly';
+    }
+
+    public function pi()
+    {
+        echo $this->name . ' pi';
+    }
+
+    public function tweet()
+    {
+        echo $this->name . ' tweet';
+    }
+
+    public function eat(string $object)
+    {
+        echo $this->name . ' eats ' . $object;
+    }
+}
